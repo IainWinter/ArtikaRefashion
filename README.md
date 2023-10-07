573a9b680792648504ae181cb21013ec609e3927
IainWinter <iainwinter1@gmail.com>
2023-10-07 18:02:45 -0400

    flask and deps in readme

---
+++ README.md
@@ -0,0 +1,2 @@
+External deps:
+    Flask
--- src/server.py
+++ src/server.py
@@ -1,1 +1,13 @@
-from flask import app
+from flask import Flask, app
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the root URL ("/") and a function to handle it
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the Flask app on the development server
+    app.run()
